673de143de9dac3c9db58026ce306ec58360fe5f
Prelang Builder <builder@prelang.com>
2015-12-25 14:07:01 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'MeasureUnit' to allow for create, update

--- app/admin/measure_unit.rb
+++ app/admin/measure_unit.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register MeasureUnit do
+  permit_params :name, :abbrev_singular, :abbrev_plural
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
